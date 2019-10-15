fdbe0f1b0472067e111fef813692688103a11454
Diego Go <diego.gomes87@gmail.com>
2019-10-15 12:50:05 -0300

    Bulding the game board

--- game.rb
+++ game.rb
@@ -2,5 +2,21 @@ class Board
   attr_accessor : board
 
   def initialize
-  
+    @board = {}
+    for cell in 1..9
+      @board[cell] = " "
+    end
+
+    @boardGrid = Array.new(3) { Array.new(3, " ")}
+    self.showInstruct
   end
+
+  def showInstruct
+    puts "\n Dont forget, You can use The number so select"
+    puts  "1 | 2 | 3",
+          "4 | 4 | 6",
+          "7 | 8 | 9"
+    puts ""
+  end
+
+  
