7cf58677276334d38a9682f263468b459d792952
Diego Go <diego.gomes87@gmail.com>
2019-10-23 18:12:52 -0300

    add initialize and move class

--- lib/board.rb
+++ lib/board.rb
@@ -4,5 +4,13 @@ require_relative 'players.rb'
 require_relative 'game.rb'
 
 class Board
+  attr_accessor :cell, :counter
+  def initialize(cell = [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    @counter = 0
+    @cell = cell
+  end
 
+  def move(element, place)
+    
+  end
 end
