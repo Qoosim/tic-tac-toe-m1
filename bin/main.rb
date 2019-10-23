a5c2d9d37b265fa5320d7bd7cc72ff952485ac6f
Diego Go <diego.gomes87@gmail.com>
2019-10-23 16:23:15 -0300

    start game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,21 @@
-#!/usr/bin/env ruby
-puts "Hello world!"
-#new branch
+##!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require_relative '../lib/game.rb'
+require_relative '../lib/players.rb'
+require_relative '../lib/board.rb'
+
+# Welcome to players
+def welcome
+  puts "Welcome Players!"
+  puts "Tic Tac Toe is a game for 2 players."
+  puts "The game is played on a cell that's 3 x 3"
+  puts "If you are X, your friend? is O"
+  puts "Players take turns putting their marks in empty squares"
+  puts "The first player to get 3 marks in a row WIN"
+  puts "When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie"
+  puts "Make sure to move in diagonal, horizontal and vertical, otherwise it's going to be an invalid movement"
+  puts "Good Luck! <_<"
+end
+welcome
