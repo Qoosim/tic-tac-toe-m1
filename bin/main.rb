ef604721548ddab96aff2e8a9ab00b6e705848da
Diego Go <diego.gomes87@gmail.com>
2019-10-25 15:44:40 -0300

    add for and simples structure about game

--- bin/main.rb
+++ bin/main.rb
@@ -1,51 +1,29 @@
 #!/usr/bin/env ruby
 
 # frozen_string_literal: true
-def welcome
-  puts "Welcome Players!"
-  puts "Tic Tac Toe is a game for 2 players."
-  puts "The game is played on a cell that's 3 x 3"
-  puts "If you are X, your friend? is O"
-  puts "Players take turns putting their marks in empty squares"
-  puts "The first player to get 3 marks in a row WIN"
-  puts "When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie"
-  puts "Make sure to move in diagonal, horizontal and vertical, otherwise it's going to be an invalid movement"
-  puts "Good Luck! <_<"
-end
-welcome
-  
-def display(cell)
-  puts '##########'
-  puts "#{cell[0][0]} | #{cell[0][1]} | #{cell[0][2]}"
-  puts '##########'
-  puts "#{cell[1][0]} | #{cell[1][1]} | #{cell[1][2]}"
-  puts '##########'
-  puts "#{cell[2][0]} | #{cell[2][1]} | #{cell[2][2]}"
-  puts '##########'
-end
-
-def name_valid?(name)
-  loop do
-    valid = name.empty?
-    return name unless valid
+p "show_rules"
+p "Welcome Players!"
+p "Tic Tac Toe is a game for 2 players."
+p "The game is played on a cell that's 3 x 3"
+p "If you are X, your friend? is O"
+p "Players take turns putting their marks in empty squares"
+p "The first player to get 3 marks in a row WIN"
+p "When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie"
+p "Make sure to move in diagonal, horizontal and vertical, otherwise it's going to be an invalid movement"
+p "select_player_mark"
+p "chomp"
+p "starting the game"
+p "show_board(game.board)"
+  for i in 0...9
+    p "player_turn #{i%2}"
+    p "select_number"  
+    num = gets.chomp  
+    p "check_choise_player(number)"  
+    p "save_choise_player(MARK, number)" 
+    p "show_board(game.board)"
+    p "game.check_winnner"  
+    p "break if game.winner "
   end
-end
-    
-def valid_element?(element)
-  loop do
-    arr = %w[X O]
-    return element if arr.include? element
-
-    puts 'Wow! It should be x or o, stay focus *_*'
-    element = gets.chomp.upcase!
-  end
-end
-
-puts "Name player: "
-player1 = gets.chomp
-
-puts "#{player1} welcome"
-
-
-
-
+  # Roy added this line, i reduced the block if else statement to one line,
+  p "show_winner(player_turn)"
+  p 'THANKS FOR PLAYING TIC TAC TOE'
